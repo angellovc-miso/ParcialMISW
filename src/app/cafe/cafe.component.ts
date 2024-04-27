deddd4b0327f3a6a8857a546e3318e281ff9bd92
angellovc <vc.angel@outlook.com>
2024-04-27 18:11:03 -0500

    Feat: adds cafe module
    in order to display a list of coffees along with its characteristics

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CafeService } from './cafe.service';
+import { Cafe } from './cafe';
 
 @Component({
   selector: 'app-cafe',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CafeComponent implements OnInit {
 
-  constructor() { }
+  cafeList: Cafe[] = [];
+  cafeByType: Record<string, Cafe[]> = {};
+  constructor(private cafeService: CafeService) { }
 
   ngOnInit() {
+    this.getCafes()
+  }
+
+  getCafes() {
+    this.cafeService.getCafes().subscribe(cafes => {
+      this.cafeList = cafes;
+      for (const cafe of this.cafeList) {
+        if (this.cafeByType[cafe.tipo] === undefined)
+          this.cafeByType[cafe.tipo] = [];
+        this.cafeByType[cafe.tipo].push(cafe);   
+      }
+    });
   }
 
 }
